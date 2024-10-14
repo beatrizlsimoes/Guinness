4d9f66661c834384ef3499ede39e36a6eaa2a383
Beatriz Simões <121191936+beatrizlsimoes@users.noreply.github.com>
2024-10-14 14:38:34 +0100

    Guinness

---
+++ guiness.py
@@ -0,0 +1,19 @@
+idade = 19
+
+print(idade)
+
+if idade > 18: 
+    print("És maior de idade. Vai votar.")
+else:
+    print("És tão novinho(a)!")
+
+gostos = ["programação","chocolate","ballet"]
+print(gostos)
+print(len(gostos))
+
+for elemento in gostos:
+    print(elemento)
+
+while 1 > 0: 
+    print("1 é maior do que 0. Wow")
+    break
